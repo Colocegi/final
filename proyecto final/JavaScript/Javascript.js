25193535e4f92ff4a2c79dbca81c555c4e93b406
Colocegi <126637211+Colocegi@users.noreply.github.com>
2023-02-28 23:53:47 -0300

    Add files via upload

---
+++ proyecto final/JavaScript/Javascript.js
@@ -0,0 +1,11 @@
+document.getElementById("boton Profesor").addEventListener("click",function() {
+    location.replace("http://paulaalbarracindesarmiento.blogspot.com")
+});
+
+document.getElementById("boton Primaria").addEventListener("click",function() {
+    location.replace("https://guia-buenos-aires.escuelasyjardines.com.ar/ESCUELA-EGB-N13-BALDOMERO-FERNANDEZ-MORENO-la-union-ezeiza-buenos-aires-i7721.htm") 
+});
+
+document.getElementById("boton Primaria localizacion").addEventListener("click",function() {
+    location.replace("https://goo.gl/maps/zsRJqwNU1hwmzcaU6")
+});
